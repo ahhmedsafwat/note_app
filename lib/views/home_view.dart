f541a3b5dbb111ac2f76163cc4fefb8b0ca57e84
Ahmed <a01273477051@gmail.com>
2023-02-12 11:00:20 +0200

    add floating action button

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -6,8 +6,10 @@ class HomeView extends StatelessWidget {
   static String id = 'HomeView';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return Scaffold(
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      body: const NotesViewBody(),
     );
   }
 }
--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/widgets/note_item.dart';
+// import 'package:note_app/widgets/note_item.dart';
 import 'package:note_app/widgets/search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
@@ -8,14 +8,14 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [
-        const Text(
+      children: const [
+        Text(
           'Notes',
           style: TextStyle(
             fontSize: 28,
           ),
         ),
-        const Spacer(
+        Spacer(
           flex: 1,
         ),
         CustomSearchIcon(),
