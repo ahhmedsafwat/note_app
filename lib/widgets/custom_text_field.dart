a937d7dfb488089dfa1bd45da3d595f8440091d5
ahmed_safwat <a01273477051@gmail.com>
2023-02-14 06:45:07 +0200

    added text field to the note bottm sheet

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,13 +2,16 @@ import "package:flutter/material.dart";
 import "package:note_app/constants.dart";
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxlines = 1});
+  final String hint;
+  final int maxlines;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxlines,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hint,
         hintStyle: const TextStyle(color: KPrimaryColor),
         border: buildBorder(
           Colors.white,
